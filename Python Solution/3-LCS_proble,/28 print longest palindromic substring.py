f1662ae056bd0e4e6cf22c6e9669313bd74ea598
Gaurav Prajapati <gauravprajapati094@gmail.com>
2022-12-04 20:11:45 +0530

    palindrome substring pattern problem

--- Python Solution/3-LCS_proble,/28 print longest palindromic substring.py
+++ Python Solution/3-LCS_proble,/28 print longest palindromic substring.py
@@ -2,6 +2,33 @@
 
 # https://leetcode.com/problems/longest-palindromic-substring/description/
 
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False]*n for i in range(n)]
+        ans = ""
+        for g in range(n):
+            i = 0
+            for j in range(g, n):
+                if(g == 0):
+                    dp[i][j] = True
+                elif (g == 1):
+                    if (s[i] == s[j]):
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = False
+                else:
+                    if (s[i] == s[j] and dp[i+1][j-1] == True):
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = False
+                if dp[i][j] == True:
+                    ans = s[i:j+1]
+                i += 1
+        return ans
+
+
 '''
 
 # java correct solution
@@ -43,36 +70,6 @@ class Solution {
 }
 
 '''
-
-# this is same java logic python correct solution
-
-
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp = [[False]*n for i in range(n)]
-        ans = ""
-        for g in range(n):
-            i = 0
-            for j in range(g, n):
-                if(g == 0):
-                    dp[i][j] = True
-                elif (g == 1):
-                    if (s[i] == s[j]):
-                        dp[i][j] = True
-                    else:
-                        dp[i][j] = False
-                else:
-                    if (s[i] == s[j] and dp[i+1][j-1] == True):
-                        dp[i][j] = True
-                    else:
-                        dp[i][j] = False
-                if dp[i][j] == True:
-                    ans = s[i:j+1]
-                i += 1
-        return ans
-
-
 '''
 from numpy import matrix
 
