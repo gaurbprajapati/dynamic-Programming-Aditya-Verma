b26d15868db5ad2b5cde6d940efac42a47da9c74
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-01-02 18:39:11 +0530

    2d dp

--- 1D DP/4_Maximum sum of non-adjacent elements.py
+++ 1D DP/4_Maximum sum of non-adjacent elements.py
@@ -31,9 +31,42 @@ def solvetabu(ind, a, dp):
 
     return dp[len(a)-1]
 
+# space optimization
+
+
+def solvespace(ind, a):
+    prev = 0
+    prev2 = 0
+
+    for i in range(0, len(a)):
+        pick = a[i]
+        if (i > 1):
+            pick += prev2
+        notpick = 0+prev
+        curr = max(pick, notpick)
+        prev2 = prev
+        prev = curr
+    return prev
+    '''
+    -----------------OR--------------
+    prev = 0
+    prev2 = 0
+    
+    for i in range(0,len(a)):
+        pick = a[i]
+        if (i > 1):
+            pick += prev2
+        notpick = 0+prev
+        curr = max(pick, notpick)
+        prev2 = prev
+        prev = curr
+    return prev
+    '''
+
 
 n = 6
 dp = [-1 for i in range(n+1)]
 a = [5, 5, 10, 100, 10, 5]
 print(solve(n-1, a, dp))
 print(solvetabu(n-1, a, dp))
+print(solvespace(n, a))
